737453ca0d311c698b71e8cc248b7b4615b08d44
BecaLParker <beca.parker@red-gate.com>
2023-11-24 16:53:25 +0000

    Return values from functions

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-
-
-#Foreach loop
+# Foreach loop
 def print_people(*people):
     for person in people:
         print("This person is", person)
 
 
-print_people("Nick", "Michael", "Bob")
+# print_people("Nick", "Michael", "Bob")
+
+# Return values from functions
+def addition(num1, num2):
+    return num1 + num2
+
+
+sum1 = addition(1, 2)
+sum2 = addition(3, 4)
+
+print("1st sum is", sum1, "and 2nd sum is", sum2)
