28b7e37bf37574d1e97c883134cb2dcb7b839622
BecaLParker <beca.parker@red-gate.com>
2023-11-24 16:59:54 +0000

    If statements

--- main.py
+++ main.py
@@ -14,4 +14,14 @@ def addition(num1, num2):
 sum1 = addition(1, 2)
 sum2 = addition(3, 4)
 
-print("1st sum is", sum1, "and 2nd sum is", sum2)
+# print("1st sum is", sum1, "and 2nd sum is", sum2)
+
+# If, elif, else statements
+becaAwesome = True
+
+if becaAwesome == True:
+    print("Beca is awesome")
+elif becaAwesome == "Hamburger":
+    print("Why are you a hamburger Beca?")
+else:
+    print("Beca is not awesome yet")
